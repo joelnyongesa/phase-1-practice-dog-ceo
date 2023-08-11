7d622b2f2304cb279f1a6ddc5f0c0cbe8b4d60ef
joelnyongesa <joelnyongesa148@gmail.com>
2023-08-11 11:01:14 +0300

    To complete challenge 2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+window.addEventListener('load', function(){
+    fetch(imgUrl)
+    .then(res => res.json())
+    .then(dogs => appendDogs(dogs))
+    .catch(e => console.log(e))
+})
+
+function appendDogs(dogs){
+    let imageContainer = document.querySelector('#dog-breeds')
+    // console.log(dogs.message[0])
+    for(image of dogs['message']){
+        // console.log(image)
+        let dogsList = document.createElement('li')
+        dogsList.innerHTML = `
+            <img src=${image}/>
+        `
+        imageContainer.appendChild(dogsList)
+    }
+}
